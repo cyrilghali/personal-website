243cb271e6f304331409ac93749016facea43311
Cyril <cyril.ghali@hotmail.com>
2022-09-23 13:10:14 +0200

    ok

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,15 +1,15 @@
 const projectsData = [
   {
+    title: 'Laserpitch',
+    description: `No code tool that allows you to build your landing page by simply filling out a form`,
+    imgSrc: '/static/images/laserpitch.jpeg',
+    href: 'https://laserpitch.com',
+  },
+  {
     title: 'Catena',
     description: `Catena is the Bible with verse-by-verse commentary from the church fathers. This gives you insights on the scripture by early Christian theologians, whose faith is the basis of all Christians.`,
     imgSrc: '/static/images/catena.png',
     href: 'https://catenabible.com',
-  },
-  {
-    title: 'CWS',
-    description: `CWS is a custom web server made in C++`,
-    imgSrc: '/static/images/spider.jpg',
-    href: 'https://github.com/datscyril/cws',
   },
 ]
 
