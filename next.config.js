b0d2ccb15b99bb36854423cf24c1164bb5105f25
Cyril Ghali <contact@cyrilghali.fr>
2023-07-09 19:48:40 +0200

    Modify CS Header policy

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app chiffre.io;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app googletagmanager.com googletagmanager.com *.googletagmanager.com *.google-analytics.com *.gstatic.com *.googleapis.com *.google.com *.giscus.app;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
